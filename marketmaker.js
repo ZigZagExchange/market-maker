dfbd466d13b26ebed7a4edd98cb2d5be4881bb43
TrooperCrypto <trooper.crypto7@gmail.com>
2022-03-01 00:44:26 +0100

    fillOpenOrders more often
    fillOpenOrders every 400ms to match 500ms in backend

--- marketmaker.js
+++ marketmaker.js
@@ -111,7 +111,7 @@ zigzagws.on('error', console.error);
 
 function onWsOpen() {
     zigzagws.on('message', handleMessage);
-    fillOrdersInterval = setInterval(fillOpenOrders, 5000);
+    fillOrdersInterval = setInterval(fillOpenOrders, 400);
     indicateLiquidityInterval = setInterval(indicateLiquidity, 5000);
     for (let market in MM_CONFIG.pairs) {
         if (MM_CONFIG.pairs[market].active) {
