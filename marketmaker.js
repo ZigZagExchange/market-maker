05bb4aae40f64159a667221d2e9e7b36032f592d
Trooper <trooper.crypto7@gmail.com>
2022-08-08 14:30:52 +0200

    Actions Projects 2 Wiki Security Insights      Settings  add arbitrum eip712 V5

--- marketmaker.js
+++ marketmaker.js
@@ -537,41 +537,82 @@ async function submitOrder (marketId, side, price, size, expirationTimeSeconds) 
     }
 
     const account = await WALLET.getAddress();
-    const Order = {
-      makerAddress: account,
-      makerToken: makerToken,
-      takerToken: takerToken,
-      feeRecipientAddress: marketInfo.feeAddress,
-      makerAssetAmount: makerAmountBN.toString(),
-      takerAssetAmount: takerAmountBN.toString(),
-      makerVolumeFee: makerVolumeFeeBN.toString(),
-      takerVolumeFee: takerVolumeFeeBN.toString(),
-      gasFee: gasFeeBN.toString(),
-      expirationTimeSeconds: expirationTimeSeconds.toFixed(0),
-      salt: (Math.random() * 123456789).toFixed(0),
-    };
-
-    const domain = {
-      name: "ZigZag",
-      version: "4",
-      chainId: CHAIN_ID,
-    };
-
-    const types = {
-      Order: [
-        { name: "makerAddress", type: "address" },
-        { name: "makerToken", type: "address" },
-        { name: "takerToken", type: "address" },
-        { name: "feeRecipientAddress", type: "address" },
-        { name: "makerAssetAmount", type: "uint256" },
-        { name: "takerAssetAmount", type: "uint256" },
-        { name: "makerVolumeFee", type: "uint256" },
-        { name: "takerVolumeFee", type: "uint256" },
-        { name: "gasFee", type: "uint256" },
-        { name: "expirationTimeSeconds", type: "uint256" },
-        { name: "salt", type: "uint256" },
-      ],
-    };
+    let domain, Order, types
+    if (marketInfo.contractVersion === 4) {
+        Order = {
+            makerAddress: account,
+            makerToken: makerToken,
+            takerToken: takerToken,
+            feeRecipientAddress: marketInfo.feeAddress,
+            makerAssetAmount: makerAmountBN.toString(),
+            takerAssetAmount: takerAmountBN.toString(),
+            makerVolumeFee: makerVolumeFeeBN.toString(),
+            takerVolumeFee: takerVolumeFeeBN.toString(),
+            gasFee: gasFeeBN.toString(),
+            expirationTimeSeconds: expirationTimeSeconds.toFixed(0),
+            salt: (Math.random() * 123456789).toFixed(0),
+        };
+  
+        domain = {
+            name: "ZigZag",
+            version: "4",
+            chainId: CHAIN_ID,
+        };
+    
+        types = {
+            Order: [
+            { name: "makerAddress", type: "address" },
+            { name: "makerToken", type: "address" },
+            { name: "takerToken", type: "address" },
+            { name: "feeRecipientAddress", type: "address" },
+            { name: "makerAssetAmount", type: "uint256" },
+            { name: "takerAssetAmount", type: "uint256" },
+            { name: "makerVolumeFee", type: "uint256" },
+            { name: "takerVolumeFee", type: "uint256" },
+            { name: "gasFee", type: "uint256" },
+            { name: "expirationTimeSeconds", type: "uint256" },
+            { name: "salt", type: "uint256" },
+            ],
+        };
+    } else if (marketInfo.contractVersion === 5) {
+            Order = {
+            makerAddress: account,
+            makerToken: makerToken,
+            takerToken: takerToken,
+            feeRecipientAddress: marketInfo.feeAddress,
+            relayerAddress: marketInfo.relayerAddress,
+            makerAssetAmount: makerAmountBN.toString(),
+            takerAssetAmount: takerAmountBN.toString(),
+            makerVolumeFee: makerVolumeFeeBN.toString(),
+            takerVolumeFee: takerVolumeFeeBN.toString(),
+            gasFee: gasFeeBN.toString(),
+            expirationTimeSeconds: expirationTimeSeconds.toFixed(0),
+            salt: (Math.random() * 123456789).toFixed(0),
+            };
+    
+        domain = {
+            name: "ZigZag",
+            version: "5",
+            chainId: CHAIN_ID,
+        };
+    
+        types = {
+            Order: [
+            { name: "makerAddress", type: "address" },
+            { name: "makerToken", type: "address" },
+            { name: "takerToken", type: "address" },
+            { name: "feeRecipientAddress", type: "address" },
+            { name: "relayerAddress", type: "address" },
+            { name: "makerAssetAmount", type: "uint256" },
+            { name: "takerAssetAmount", type: "uint256" },
+            { name: "makerVolumeFee", type: "uint256" },
+            { name: "takerVolumeFee", type: "uint256" },
+            { name: "gasFee", type: "uint256" },
+            { name: "expirationTimeSeconds", type: "uint256" },
+            { name: "salt", type: "uint256" },
+            ],
+        };
+    }
 
     const signature = await WALLET._signTypedData(domain, types, Order);
 
