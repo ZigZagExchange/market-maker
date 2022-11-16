3f04e4136d3f92f8db571df3d1f5bbcb8565f6e2
TrooperCrypto <trooper.crypto7@gmail.com>
2022-11-17 00:34:10 +0100

    remove old order struct

--- marketmaker.js
+++ marketmaker.js
@@ -660,76 +660,7 @@ async function getOrderCalldata(
 
   const userAccount = await getMMBotAccount();
   let domain, Order, types;
-  if (Number(marketInfo.contractVersion) === 6) {
-    let gasFeeBN;
-    if (side === "s") {
-      gasFeeBN = ethers.utils.parseUnits(
-        Number(marketInfo.baseFee).toFixed(marketInfo.baseAsset.decimals),
-        marketInfo.baseAsset.decimals
-      );
-    } else {
-      gasFeeBN = ethers.utils.parseUnits(
-        Number(marketInfo.quoteFee).toFixed(marketInfo.quoteAsset.decimals),
-        marketInfo.quoteAsset.decimals
-      );
-    }
-
-    // size check
-    if (makerVolumeFeeBN.gte(takerVolumeFeeBN)) {
-      balanceBN = balanceBN.sub(gasFeeBN).sub(makerVolumeFeeBN);
-    } else {
-      balanceBN = balanceBN.sub(gasFeeBN).sub(takerVolumeFeeBN);
-    }
-    const delta = sellAmountBN.mul("100000").div(balanceBN).toNumber();
-    if (delta > 100100) {
-      // 100.1 %
-      throw new Error(`Amount exceeds balance.`);
-    }
-    // prevent dust issues
-    if (delta > 99990) {
-      // 99.9 %
-      sellAmountBN = balanceBN;
-      buyAmountBN = buyAmountBN.mul(100000).div(delta);
-    }
-
-    Order = {
-      user: userAccount,
-      sellToken: sellToken,
-      buyToken: buyToken,
-      feeRecipientAddress: marketInfo.feeAddress,
-      relayerAddress: marketInfo.relayerAddress,
-      sellAmount: sellAmountBN.toString(),
-      buyAmount: buyAmountBN.toString(),
-      makerVolumeFee: makerVolumeFeeBN.toString(),
-      takerVolumeFee: takerVolumeFeeBN.toString(),
-      gasFee: gasFeeBN.toString(),
-      expirationTimeSeconds: expirationTimeSeconds.toFixed(0),
-      salt: (Math.random() * 123456789).toFixed(0),
-    };
-
-    domain = {
-      name: "ZigZag",
-      version: "6",
-      chainId: CHAIN_ID,
-    };
-
-    types = {
-      Order: [
-        { name: "user", type: "address" },
-        { name: "sellToken", type: "address" },
-        { name: "buyToken", type: "address" },
-        { name: "feeRecipientAddress", type: "address" },
-        { name: "relayerAddress", type: "address" },
-        { name: "sellAmount", type: "uint256" },
-        { name: "buyAmount", type: "uint256" },
-        { name: "makerVolumeFee", type: "uint256" },
-        { name: "takerVolumeFee", type: "uint256" },
-        { name: "gasFee", type: "uint256" },
-        { name: "expirationTimeSeconds", type: "uint256" },
-        { name: "salt", type: "uint256" },
-      ],
-    };
-  } else if (Number(marketInfo.contractVersion) == 2.0) {
+  if (Number(marketInfo.contractVersion) == 2.0) {
     // size check
     if (makerVolumeFeeBN.gte(takerVolumeFeeBN)) {
       balanceBN = balanceBN.sub(makerVolumeFeeBN);
@@ -811,6 +742,16 @@ function getPairs() {
 }
 
 function getCurrencyInfo(currency) {
+  if (currency === VAULT_TOKEN_SYMBOL) {
+    return {
+      id: VAULT,
+      address: VAULT,
+      symbol: VAULT_TOKEN_SYMBOL,
+      decimals: VAULT_DECIMALS,
+      usdPrice: 1
+    }    
+  }
+
   const pairs = getPairs();
   for (let i = 0; i < pairs.length; i++) {
     const pair = pairs[i];
@@ -876,8 +817,8 @@ async function getBalanceOfCurrency(token, contractAddress) {
     console.log(e);
     return result;
   }
+}
 
-  async function getMMBotAccount() {
-    return VAULT ? VAULT : WALLET.getAddress();
-  }
+async function getMMBotAccount() {
+  return VAULT ? VAULT : WALLET.getAddress();
 }
